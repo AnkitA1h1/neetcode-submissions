665872e9d74609a14cb223b617da0c057663764f
AnkitA1h1 <48407026+AnkitA1h1@users.noreply.github.com>
2026-08-27 09:58:15 +0530

    Add: valid-word-abbreviation - submission-5

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-5.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+
+        int n=0;
+        char[] arr = abbr.toCharArray();
+        for (int i=0, j=0;i<abbr.length();i++,j++){
+           
+            while(i<abbr.length() && Character.isDigit(abbr.charAt(i))){
+                n = (n*10)+ (abbr.charAt(i)-'0');
+                if(n==0) return false;
+                i++;
+            }
+            j = j+n;
+            if(j==word.length() && i == abbr.length()) return true;
+            if(j>=word.length() || abbr.charAt(i) != word.charAt(j)) return false;
+
+            n=0;
+        }
+        return true;
+        
+    }
+}
