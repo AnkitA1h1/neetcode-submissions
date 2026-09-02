77e08124da1be1f5c1b236cb7b67888718e5edea
AnkitA1h1 <48407026+AnkitA1h1@users.noreply.github.com>
2026-09-02 11:13:05 +0530

    Add: crawler-log-folder - submission-3

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> st = new Stack<>();
+        int depth =0;
+        
+        for(String log : logs){
+            if(log.equals("./")){
+                continue;
+            }else if(log.equals("../") && depth>0 /*!st.isEmpty()*/){
+                //st.pop();
+                depth--;
+                continue;
+            }else if(!log.equals("../"))
+            depth++;
+            //st.push(log);
+        }
+
+        return depth;
+    }
+}
