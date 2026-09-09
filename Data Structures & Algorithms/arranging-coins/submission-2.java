20b77dbbf2e591e4f5e33197e8508bf369a6e078
AnkitA1h1 <48407026+AnkitA1h1@users.noreply.github.com>
2026-09-09 10:41:51 +0530

    Add: arranging-coins - submission-2

---
+++ Data Structures & Algorithms/arranging-coins/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            long coins = (long) mid * (mid + 1) / 2;
+
+            if (coins == n) {
+                return mid;
+            } else if (coins < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return end;
+    }
+}
